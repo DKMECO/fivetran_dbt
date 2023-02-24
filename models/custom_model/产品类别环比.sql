6fea1b70900e29c03ca710e1d91736f1a292df56
root <root@ip-172-31-13-110.cn-northwest-1.compute.internal>
2023-02-24 06:25:33 +0000

    更新

---
+++ models/custom_model/产品类别环比.sql
@@ -0,0 +1,26 @@
+{{ config(materialized='view') }}
+select
+ 年月
+,类别
+,销售数量
+,lag(销售数量) over (partition by 类别 order by 年月) as 销售数量_上月
+,销售数量/lag(销售数量) over (partition by 类别 order by 年月)-1 as 销售数量_环比
+,销售额
+,lag(销售额) over (partition by 类别 order by 年月) as 销售额_上月
+,销售额/lag(销售额) over (partition by 类别 order by 年月)-1 as 销售额_环比
+,销售利润
+,lag(销售利润) over (partition by 类别 order by 年月) as 销售利润_上月
+,销售利润/lag(销售利润) over (partition by 类别 order by 年月)-1 as 销售利润_环比
+from(
+select
+ date_trunc('month',cast(order_date_c as date))::DATE as 年月
+,category_c as 类别
+,sum(cast(quantity_c as decimal)) as 销售数量
+,sum(cast(sales_c as decimal)) as 销售额
+,sum(cast(profit_c as decimal)) as 销售利润
+from ods_salesforce_sandbox.shop_order_c t1
+group by
+ date_trunc('month',cast(order_date_c as date))
+,category_c
+)a
+
