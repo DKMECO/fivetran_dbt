fb29d0a97e2c943e5b85c1b872f9d7b9956253bc
root <root@ip-172-31-13-110.cn-northwest-1.compute.internal>
2023-02-24 03:26:46 +0000

    更新

--- models/custom_model/区域经理业绩.sql
+++ models/custom_model/区域经理业绩.sql
@@ -6,7 +6,7 @@ select
 ,sum(cast(t1.sales_c as decimal)) as 销售额
 ,sum(cast(t1.profit_c as decimal)) as 销售利润
 from {{ref('dm_shop_detail')}} t1
-left join {{ref('dm_people_adujst')}} t2 on t2.region=t1.region_c
+left join {{ref('dm_people_adjust')}} t2 on t2.region=t1.region_c
 group by
  date_trunc('year',cast(t1.order_date_c as date))
 ,t1.regional_manager
